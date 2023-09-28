7714697b01c9594c1b285322761ec611acac909c
salineroalex <alexsalineronegro@gmail.com>
2023-09-28 19:21:16 +0200

    Last javadoc done on Model.java

--- src/controller/Model.java
+++ src/controller/Model.java
@@ -5,5 +5,9 @@ package controller;
  * @author Irati Garzón
  */
 public interface Model {
+    /**
+     * Interface for the model
+     * @return Returns the implementation.
+     */
     public String getGreeting();
 }
